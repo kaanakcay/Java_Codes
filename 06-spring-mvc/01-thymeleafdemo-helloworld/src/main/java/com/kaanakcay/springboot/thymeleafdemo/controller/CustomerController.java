f41e74de4447cd8fb637fcae29b563280ba82b9d
kaanakcay <akcay.kaandeniz@gmail.com>
2025-03-11 13:37:45 +0300

    added string trim with initBinder

--- 06-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/kaanakcay/springboot/thymeleafdemo/controller/CustomerController.java
+++ 06-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/kaanakcay/springboot/thymeleafdemo/controller/CustomerController.java
@@ -3,15 +3,28 @@ package com.kaanakcay.springboot.thymeleafdemo.controller;
 
 import com.kaanakcay.springboot.thymeleafdemo.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    //whitespace engellemek icin gelen tum requestleri elden geciren InitBinder
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+
+    }
+
 
     @GetMapping("/")
     public String showForm(Model theModel){
@@ -22,6 +35,7 @@ public class CustomerController {
 
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,BindingResult theBindingResult){ //binding result hold the result of the validation
+
 
         if(theBindingResult.hasErrors()){
             return "customer-form";
