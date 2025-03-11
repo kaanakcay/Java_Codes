76b59e5f1367777972d935b22c51020181c3f0f3
kaanakcay <akcay.kaandeniz@gmail.com>
2025-03-11 13:34:49 +0300

    customer login system added (validation example)

---
+++ 06-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/kaanakcay/springboot/thymeleafdemo/controller/CustomerController.java
@@ -0,0 +1,34 @@
+package com.kaanakcay.springboot.thymeleafdemo.controller;
+
+
+import com.kaanakcay.springboot.thymeleafdemo.model.Customer;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+
+    @GetMapping("/")
+    public String showForm(Model theModel){
+        theModel.addAttribute("customer", new Customer());
+
+        return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,BindingResult theBindingResult){ //binding result hold the result of the validation
+
+        if(theBindingResult.hasErrors()){
+            return "customer-form";
+        }else{
+            return "customer-confirmation";
+        }
+
+
+    }
+}
